b0205eecbcc82754bae334b4e27c2a8919885e92
EC2 Default User <ec2-user@ip-172-31-5-96.ap-northeast-1.compute.internal>
2023-11-01 11:49:57 +0000

    Refile.secter_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '45e31ef006a073e959dabcc88a82029e52ecb51602b50ebbff4aa6a56206cddb508b2b4d7b6630eddfffbb13e4ad08544de1e93ac033951b19bad8ab34ef9c74'
